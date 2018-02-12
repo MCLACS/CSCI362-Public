12becb95c83d9db316fda8da1266c6f6b00739c0
Mark Cohen <mark.cohen@mcla.edu>
2018-02-12 14:20:44 +0000

    dfsf

--- IPC/FileSearch/src/Master.java
+++ IPC/FileSearch/src/Master.java
@@ -8,18 +8,22 @@ public class Master
 
   public static void main(String args[]) throws Exception
   {
-    new Master().go(args);
+    new Master().go();
   }
 
-  private void go(String args[]) throws Exception
+  private void go() throws Exception
   {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter keyword to search for:");
+    String keyword = in.nextLine();
+    
     List<Process> list = new ArrayList<Process>();
     for (int i = 0; i < 26; i++)
     {
       System.out.printf("Starting process for files starting with %s ...%n",
         alpha[i]+"");
       Process p = new ProcessBuilder("java", "Worker", alpha[i]+"",
-        args[0]).start();
+        keyword).start();
       list.add(p);
     }
 
@@ -31,7 +35,9 @@ public class Master
 
   private void search(Process p) throws Exception
   {
+    System.out.printf("Waiting for worker...%n");
     p.waitFor();
+    System.out.printf("Worker done ...%n");
     BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
     String line = br.readLine();
     while (line != null)
